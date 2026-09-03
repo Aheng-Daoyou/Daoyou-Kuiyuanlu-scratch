6736f0cd1375d4173f288d9987fac93edb6a769d
zihengzhao55-code <zihengzhao55@gmail.com>
2026-09-03 19:20:49 +0800

    seed 23

---
+++ src/react-app/components/feature/consumables/index.ts
@@ -0,0 +1,32 @@
+export {
+  ConsumableDetailModal,
+} from './ConsumableDetailModal';
+export {
+  ConsumableListCard,
+  type ConsumableListCardProps,
+} from './ConsumableListCard';
+export { getConsumableListSummary } from './consumableListSummary';
+export {
+  describePillOperation,
+  getPillFamilyLabel,
+  PillAppearanceMark,
+  PillDetailGroups,
+  PillKeywordBadges,
+  PillKeywordLine,
+  PillSummary,
+  toPillDisplayModel,
+  type PillDetailGroup,
+  type PillDisplayModel,
+} from './pillDisplay';
+export {
+  buildTalismanDetailText,
+  buildTalismanUseConfirmText,
+  getTalismanActionHref,
+  getTalismanActionLabel,
+  getTalismanScenarioLabel,
+  getTalismanUsageHint,
+  isAttributeResetTalisman,
+  isSectMeridianResetTalisman,
+  isSectTransferTalisman,
+  isQiRestoreTalisman,
+} from './talismanDisplay';
